8b3c0d3737abe25e2af56a2da49bc1df9f945874
axetroy <troy450409405@gmail.com>
2018-01-24 15:43:02 +0800

    update

--- app/server.ts
+++ app/server.ts
@@ -1,69 +1,78 @@
 /**
  * Created by axetroy on 17-7-13.
  */
-import { EventEmitter } from 'events';
-const path = require('path');
-import * as express from 'express';
-const cookieParser = require('cookie-parser');
-const timeout = require('connect-timeout');
-const helmet = require('helmet');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+import { EventEmitter } from "events";
+const path = require("path");
+import * as express from "express";
+const cookieParser = require("cookie-parser");
+const timeout = require("connect-timeout");
+const helmet = require("helmet");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-import CONFIG from './config';
-import sequelize from './postgres/index';
+import CONFIG from "./config";
+import sequelize from "./postgres/index";
 
 // Routers
-import GraphqlRouter from './routers/graphql';
-import exportsRouter from './routers/exports';
+import GraphqlRouter from "./routers/graphql";
+import exportsRouter from "./routers/exports";
 
-import { initUser } from './controllers/user';
+import { initUser } from "./controllers/user";
 
 const app: express.Application = express();
 
 // 端口监听
 const SERVER_PORT: number = Number(process.env.PORT) || 3000;
 
-process.on('uncaughtException', function(err) {
-  console.error('Error caught in uncaughtException event:', err);
+process.on("uncaughtException", function(err) {
+  console.error("Error caught in uncaughtException event:", err);
 });
 
-process.on('unhandledRejection', (reason, p) => {
-  console.log('Unhandled Rejection at:', p, 'reason:', reason);
+process.on("unhandledRejection", (reason, p) => {
+  console.log("Unhandled Rejection at:", p, "reason:", reason);
 });
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true, type: 'application/x-www-form-urlencoded' }));
-// parse application/json
-app.use(bodyParser.json());
-app.use(bodyParser.text());
-app.use(timeout('120s'));
-app.use(cookieParser());
-app.use(helmet());
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
 app.use(
   cors({
-    origin: function(ctx) {
-      return ctx.headers.origin;
-    },
-    allowedHeaders: ['Content-Type', 'Content-Length', 'Authorization'],
+    origin: true,
     credentials: true,
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    allowHeaders: [
+      "Origin",
+      "Accept-Language",
+      "X-Requested-with",
+      "Content-Type",
+      "Accept",
+      "Authentication",
+      "Authorization",
+      "X-Real-IP",
+      "X-Server",
+      "X-Forward-Ip"
+    ],
+    allowMethods: ["GET", "HEAD", "OPTIONS", "POST"]
   })
 );
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true, type: "application/x-www-form-urlencoded" }));
+// parse application/json
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(timeout("120s"));
+app.use(cookieParser());
+app.use(helmet());
+
 app.use(function(req: any, res: any, next: Function) {
-  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-  res.header('Expires', '-1');
-  res.header('Pragma', 'no-cache');
+  res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
+  res.header("Expires", "-1");
+  res.header("Pragma", "no-cache");
   next();
 });
 
 class Program extends EventEmitter {
-  private ERROR: string = 'error';
+  private ERROR: string = "error";
   constructor() {
     super();
     this.on(this.ERROR, (err: Error) => {
@@ -114,17 +123,17 @@ class Program extends EventEmitter {
    */
   async onServer(port: number = SERVER_PORT): Promise<any> {
     // graphql 接口
-    app.use('/api', GraphqlRouter());
-    app.use('/api', exportsRouter());
+    app.use("/api", GraphqlRouter());
+    app.use("/api", exportsRouter());
 
     app.use((req, res, next) => {
-      res.status(404).json({ error: [{ message: 'Invalid url' }] });
+      res.status(404).json({ error: [{ message: "Invalid url" }] });
     });
 
     // error handler
     app.use(function(err: any, req: any, res: any, next: Function) {
       res.status(err.status || 500);
-      res.render('error', {
+      res.render("error", {
         message: err.message,
         error: CONFIG.isProduction ? {} : err
       });
